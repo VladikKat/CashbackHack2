ac7b52a07445eefd9e668d8db2d4112f9a67f671
Name Surname <vladislavabasarina8@gmail.com>
2024-01-26 11:59:57 +0300

    project init

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,22 @@
+package ru.netology.service;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @org.junit.Test
+    public void remain() {
+        CashbackHackService2 service = new CashbackHackService2();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+    @org.junit.Test
+    public void remain2() {
+        CashbackHackService2 service = new CashbackHackService2();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual, expected);
+}
